71402ad7a899501d021c859345be6f37157ce29b
TrMiDuc <ductruong9922@gmail.com>
2025-08-29 10:30:42 +0700

    api documentation
    api doc
    
    api doc

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BillingController;
 use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Controllers\Api\UserApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,11 +31,22 @@ Route::prefix('products')->group(function () {
     Route::get('/search', [ProductApiController::class, 'search'])->name('api.products.search');
     Route::get('/category/{categoryId}', [ProductApiController::class, 'getByCategory'])->name('api.products.by-category');
     Route::get('/{id}', [ProductApiController::class, 'show'])->name('api.products.show');
-    
     // Protected routes (require authentication)
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/', [ProductApiController::class, 'store'])->name('api.products.store');
         Route::put('/{id}', [ProductApiController::class, 'update'])->name('api.products.update');
         Route::delete('/{id}', [ProductApiController::class, 'destroy'])->name('api.products.destroy');
     });
+});
+
+Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
+    Route::get('/users', [UserApiController::class, 'index'])->name('api.admin.users.index');
+    Route::post('/users', [UserApiController::class, 'store'])->name('api.admin.users.store');
+    Route::get('/users/{user}', [UserApiController::class, 'show'])->name('api.admin.users.show');
+    Route::put('/users/{user}', [UserApiController::class, 'update'])->name('api.admin.users.update');
+    Route::delete('/users/{user}', [UserApiController::class, 'destroy'])->name('api.admin.users.destroy');
+
+    // extra route for deleting images
+    Route::delete('/users/{userId}/images/{imageId}', [UserApiController::class, 'destroyUserImage'])
+        ->name('api.admin.users.destroyImage');
 });
